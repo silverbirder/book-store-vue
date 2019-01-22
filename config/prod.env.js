3344140313e5d28900ed9966dcd25668632fbcdb
shibamoto <silverbirder@gmail.com>
2019-01-22 23:12:16 +0900

    update layout

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,5 +4,5 @@ module.exports = {
   VUE_APP_ALGOLIA_INDEX_KEY: '"'+process.env.ALGOLIA_INDEX_KEY+'"',
   VUE_APP_ALGOLIA_APP_KEY: '"'+process.env.ALGOLIA_APP_KEY+'"',
   VUE_APP_ALGOLIA_SEARCH_KEY: '"'+process.env.ALGOLIA_SEARCH_KEY+'"',
-  VUE_APP_BOOK_API_URL: '"https://book-store-go-lifmwicrvo.now.sh"'
+  VUE_APP_BOOK_API_URL: '"https://book-store-go.now.sh"'
 }
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,14 +4,53 @@
     :api-key="API_KEY"
     :index-name="INDEX_NAME"
   >
-    <ais-search-box></ais-search-box>
-    <ais-results>
+    <div class="md-field">
+      <ais-input placeholder="Find books..." class="md-input" autofocus></ais-input>
+    </div>
+    <ais-results class="md-layout md-gutter md-alignment-center">
       <template slot-scope="{ result }">
-        <h2>
-          <ais-highlight :result="result" attribute-name="title"></ais-highlight>
-        </h2>
+        <md-card class="md-layout-item md-medium-size-10 md-small-size-25 md-xsmall-size-50">
+          <md-card-area md-inset>
+            <md-card-media>
+              <img :src="result.cover">
+            </md-card-media>
+
+            <md-card-header>
+              <div class="md-title">{{ result.title }}</div>
+              <div class="md-subhead">{{ result.author }}</div>
+            </md-card-header>
+
+            <md-card-content>
+               {{ result.publisher}}, {{ result.pubdate }}
+            </md-card-content>
+
+            <md-card-expand>
+              <md-card-actions md-alignment="space-between">
+                <div>
+                  <md-button>Edit</md-button>
+                </div>
+                <md-card-expand-trigger>
+                  <md-button class="md-icon-button">
+                    <md-icon>keyboard_arrow_down</md-icon>
+                  </md-button>
+                </md-card-expand-trigger>
+              </md-card-actions>
+              <md-card-expand-content>
+                <md-card-content>
+                  {{ result.textContent[0].Text }}
+                </md-card-content>
+              </md-card-expand-content>
+            </md-card-expand>
+
+          </md-card-area>
+        </md-card>
       </template>
     </ais-results>
+    <ais-no-results>
+      <template slot-scope="props">
+        No products found for <i>{{ props.query }}</i>.
+      </template>
+    </ais-no-results>
   </ais-index>
 </template>
 
@@ -22,8 +61,8 @@
       return {
         'APP_ID': process.env.VUE_APP_ALGOLIA_APP_KEY,
         'API_KEY': process.env.VUE_APP_ALGOLIA_SEARCH_KEY,
-        'INDEX_NAME': process.env.VUE_APP_ALGOLIA_INDEX_KEY,
+        'INDEX_NAME': process.env.VUE_APP_ALGOLIA_INDEX_KEY
       }
-    }
+    },
   }
 </script>
